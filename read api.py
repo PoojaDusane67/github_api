288b88bd9b81fd4857bc2efd4374328d28ada284
Pooja Dusane <dusanepooja91@gmail.com>
2025-10-25 00:38:06 +0530

    test 2

--- read api.py
+++ read api.py
@@ -5,6 +5,6 @@ response = requests.get("https://jsonplaceholder.typicode.com/users")
 data = response.json()
 
 df = pd.DataFrame(data)
-d=df[["id","namme","email"]]
+d=df[["id","name","email"]]
 
 print(df)
